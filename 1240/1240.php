9399312373d21b7355abd7d405097c408bc523bf
minhphuc1909 <huynhminhphuc1909@gmail.com>
2018-05-14 16:16:14 +0700

    commit

--- 1240/1240.php
+++ 1240/1240.php
@@ -15,16 +15,18 @@ if (!class_exists('lessc')) {
 $less = new lessc;
 $less->compileFile('less/1240.less', 'css/1240.css');
 ?>
-<html>
+<html lang="en">
     <head>
+
         <title>1240</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-       <link href="<?php echo $url_path ?>/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+
+        <link href="<?php echo $url_path ?>/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
         <link rel="stylesheet" href="<?php echo $url_path ?>/css/swiper.min.css">
         <link href="<?php echo $url_path ?>/css/1240.css" rel="stylesheet" type="text/css"/>
         <link rel="stylesheet" type="text/css" href="<?php echo $url_path ?>/css/font-awesome.min.css">
-        <script src="<?php echo $url_path ?>/js/m1-slide-js_1.js"></script>
+        
         <script src="<?php echo $url_path ?>/js/jquery-2.1.4.min.js"></script>
         <script src="<?php echo $url_path ?>/js/swiper.min.js"></script>
     </head> 
