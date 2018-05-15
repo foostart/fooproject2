b7f811c9281fc49f39fadb7260d145bf6f708c20
alvindragoon <33717480+alvindragoon@users.noreply.github.com>
2018-05-15 16:45:52 +0700

    ghep page icon

---
+++ pages/Icon.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Icon</title>
+
+	<link href="../1248/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+	<link href="../1248/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+
+	<link  href="../1234/css/1234.css" rel="stylesheet" type="text/css"/>
+	<link  href="../1244/css/1244.css" rel="stylesheet" type="text/css"/>
+     <link href="../1248/css/1248.css" rel="stylesheet" type="text/css"/>
+
+
+	<script src="../1234/js/jquery-2.1.4.min.js" ></script>
+	<script src="../1248/js/bootstrap.min.js" ></script>
+    <script src="../1234/js/1234.js" ></script>
+
+    <script src="../1248/js/amination.js"></script>
+
+
+
+</head>
+<body>
+	<?php include '../1234/1234-content.php'; ?>
+	<?php include '../1248/1248-content.php'; ?>
+	<?php include '../1244/1244-content.php'; ?>
+
+</body>
+</html>
