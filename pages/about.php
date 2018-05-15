474e326600621b93686579f749e628d244249b27
vucd1785 <36876695+vucd1785@users.noreply.github.com>
2018-05-15 15:22:01 +0700

    About: done

---
+++ pages/about.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="vi">
+    <head>
+        <title>About page</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=100" />
+        <meta http-equiv="REFRESH" content="1800" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-title" content="Tai lieu web" />
+        <meta name="description" content="Tai lieu web" />
+        <meta name="keywords" content="Tai lieu web"/>
+
+        <link rel="shortcut icon" href="blocks/2034/images/favicon.ico"/>
+
+        <link href="../1246/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="../1246/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+        <link rel="stylesheet" href="../1249/css/swiper.min.css" type="text/css">
+
+
+        <link href="../1246/css/1246.css" rel="stylesheet" type="text/css"/> 
+        <link href="../1247/css/1247.css" rel="stylesheet" type="text/css"/>
+        <link href="../1253/css/1253.css" rel="stylesheet" type="text/css"/>
+         <link href="../1249/css/1249.css" rel="stylesheet" type="text/css" /> 
+         <link href="../1250/css/1250.css" rel="stylesheet" type="text/css" />
+         <link href="../1244/css/1244.css" rel="stylesheet" type="text/css"/> 
+
+        
+        
+        
+
+        <script src="../1246/js/jquery-2.1.4.min.js" type="text/javascript"></script>
+        <script src="../1246/js/bootstrap.min.js" type="text/javascript"></script>
+         <script src="../1246/js/1246.js" type="text/javascript"></script>
+          <script src="../1249/js/1249.js" type="text/javascript"></script>
+          <script  src="../1249/js/swiper.min.js"></script>
+          <script src="../1249/js/starrating.js"></script>
+        
+    </head>
+    <body>
+        <?php  include '../1246/1246-content.php'; ?>
+        <?php  include '../1247/1247-content.php'; ?>
+        <?php  include '../1253/1253-content.php'; ?>
+        <?php  include '../1249/1249-content.php'; ?>
+        <?php  include '../1250/1250-content.php'; ?>
+        <?php  include '../1244/1244-content.php'; ?>
+
+    </body>
+</html>
