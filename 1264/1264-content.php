af9e79e9f7528e40f85780ab30b53a22c1840ed2
thinh1234567 <36876783+thinh1234567@users.noreply.github.com>
2018-05-15 14:11:58 +0700

    commit viethoa 1249 - 1264

--- 1264/1264-content.php
+++ 1264/1264-content.php
@@ -1,4 +1,3 @@
-
 <?php
 $url_host = $_SERVER['HTTP_HOST'];
 
@@ -13,19 +12,13 @@ $url_path = $url_host . $matches[1][0];
 $url_path = str_replace('\\', '/', $url_path);
 
 ?>
-<div class="type-1264">
-    <div class="container">
-    <div class="parallax-bg-wrapper">
-        <img class="image-size" src="images/section-bg.jpg">
-        <div>
-            <h2 class="top">Breakfast & Brunch</h2>
-        <hr  class="hr-class top">
-        <div class="text">
-        <span>Masonry Portfolio Style</span>
-        </div>
-        </div>
-        
-    </div>
-        </div>
-</div>
+    <div class="type-1264">
 
+        <div class="parallax-bg-wrapper">
+            <img class="image-size" src="<?php echo $url_path ?>/images/section-bg.jpg">
+            <div>
+                <h2 class="top">Bữa Sáng & Buổi ăn Nhẹ</h2>
+
+            </div>
+        </div>
+    </div>
--- 1264/1264.php
+++ 1264/1264.php
@@ -15,17 +15,20 @@ if (!class_exists('lessc')) {
 $less = new lessc;
 $less->compileFile('less/1264.less', 'css/1264.css');
 ?>
-<html>
+    <html>
+
     <head>
         <meta charset="UTF-8">
         <title>1264</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <link href="<?php echo $url_path ?>/css/1264.css" rel="stylesheet" type="text/css"/>
-        <link href="<?php echo $url_path ?>/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-        <link href="<?php echo $url_path ?>/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>  
+        <link href="<?php echo $url_path ?>/css/1264.css" rel="stylesheet" type="text/css" />
+        <link href="<?php echo $url_path ?>/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="<?php echo $url_path ?>/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
 
     </head>
+
     <body>
         <?php include './1264-content.php'; ?>
-    </body> 
-</html>
+    </body>
+
+    </html>
