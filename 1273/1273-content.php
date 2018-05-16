45e548a8cf9bd53e96b6b692dd7752f3a50ad1c9
Mihi <ngochiensunflowers@gmail.com>
2018-05-16 10:41:38 +0700

    commit

--- 1273/1273-content.php
+++ 1273/1273-content.php
@@ -12,148 +12,7 @@ $url_path = $url_host . $matches[1][0];
 $url_path = str_replace('\\', '/', $url_path);
 ?>
 
-<!-- <div class="type-1273">
-   <header>
-       <div class="header-navigator-caption">
-            <div class="container">
-                <div class="header-nav">
-                    <div class="logo">
-                        <a href="#">Délicious</a>
-                    </div>
-                    <div class="menu-nav">
-                        <ul>
-                            <li>
-                                <a href="#">Homes</a>
-                                <ul class="dropdown">
-                                    <li>
-                                        <a href="#">Gourmet</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Coffee</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Sushi</a>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li>
-                                <a href="#">About</a>
-                                <ul class="dropdown">
-                                    <li>
-                                        <a href="#">People</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Geustbook</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">FAQ</a>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li>
-                                <a href="#">Menu</a>
-                                <ul class="dropdown">
-                                    <li>
-                                        <a href="#">Expressive</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Handsome</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Classic</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Gallery</a>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li>
-                                <a href="#">Reservation</a>                       
-                            </li>
-                            <li>
-                                <a href="#">Gallery</a>
-                                <ul class="dropdown">
-                                    <li>
-                                        <a href="#">Restaurant</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Coffee Shop</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Sushi Bar</a>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li>
-                                <a href="#">Blog</a>
-                                <ul class="dropdown">
-                                    <li>
-                                        <a href="#">Healthy Food</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Recipes</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Cooking</a>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li>
-                                <a href="#">Features</a>
-                                <ul class="dropdown">
-                                    <li>
-                                        <a href="#">Why Buy?</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Theme Options</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Coming Soon</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Icons</a>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li>
-                                <a href="#">Shop</a>
-                            </li>
-                            <li>
-                                <a href="#">Contact</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="navi-cart">
-                        <div class="shop-cart">
-                            <a href="#"><i class="fa fa-shopping-bag"></i></a>
-                            <span>0</span>
-                            <div class="list-cart">
-                                <span>No product in the cart</span>
-                            </div>
-                        </div>
-                        <div id="icon-responsive">
-                            <i class="fa fa-bars"></i>
-                        </div>
-                        <div class="search">
-                            <i class="fa fa-search icon-search"></i>
-                            <form class="search-panel" method="post">
-                                <input type="search" class="menu-search-text" placeholder="Please input search...">
-                                <button class="fa fa-search menu-search-btn"></button>
-                            </form>
-                        </div>                        
-                    </div>
-                </div>
-                <div class="header-caption">
-                    <div class="m1-banner-caption-layer layer-0">People</div>
-                    <div class="m1-header-line-layer layer-a"></div>
-                    <div class="m1-banner-caption-layer layer-1">Team of professionals</div>
-                </div>
-            </div>
-       </div>
-   </header>
-</div>
- -->
- <div class="type-1273">
+ <!-- <div class="type-1273">
     <div class="header-top">
         <header class="main-header">
             <div class="limit-wrapper">
@@ -212,16 +71,6 @@ $url_path = str_replace('\\', '/', $url_path);
                                     <li><a href="#">Why Buy?</a></li>
                                     <li><a href="#">Theme Options</a></li>
                                     <li><a href="#">Coming Soon</a></li>
-                                    <li>
-                                        <a>Blog post  ></a>
-                                        <ul class="m1-navi-menus-subMenu-1">
-                                            <li><a href="#">No Sidebar</a></li>
-                                            <li><a href="#">Left Sidebar</a></li>
-                                            <li><a href="#">Right Sidebar</a></li>
-                                            <li><a href="#">Left &amp; Right Sidebar</a></li>
-                                            <li><a href="#">Top Widgets</a></li> 
-                                        </ul>
-                                    </li>
                                     <li><a href="#">Icons</a></li>
                                 </ul>
                             </li>
@@ -269,4 +118,109 @@ $url_path = str_replace('\\', '/', $url_path);
             </div>
         </div>
     </div>
+</div> -->
+
+
+<div class="type-1273">
+    <header>
+        <div class="header-top">
+            <div class="navi-logo">
+                <a href="#">Délicious</a>
+            </div>
+            <div class="m1-navi-menus toggle-transition">
+                <ul>
+                    <li>
+                        <a href="#" class="m1-navi-menus-Activity">Homes</a>
+                        <ul class="m1-navi-menus-subMenu">
+                            <li><a href="#" >Gourmet</a></li>
+                            <li><a href="#" class="m1-navi-menus-Activity">Coffee</a></li>
+                            <li><a href="#">Sushi</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">About</a>
+                        <ul class="m1-navi-menus-subMenu">
+                            <li><a href="#">People</a></li>
+                            <li><a href="#">Guestbook</a></li>
+                            <li><a href="#">FAQ</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">Menu</a>
+                        <ul class="m1-navi-menus-subMenu">
+                            <li><a href="#">Expressive</a></li>
+                            <li><a href="#">Handsome</a></li>
+                            <li><a href="#">Classic</a></li>
+                            <li><a href="#">Gallery</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Reservation</a></li>
+                    <li>
+                        <a href="#">Gallery</a>
+                        <ul class="m1-navi-menus-subMenu">
+                            <li><a href="#">Restaurant</a></li>
+                            <li><a href="#">Coffee Shop</a></li>
+                            <li><a href="#">Sushi Bar</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">Blog</a>
+                        <ul class="m1-navi-menus-subMenu">
+                            <li><a href="#">Healthy Food</a></li>
+                            <li><a href="#">Recipes</a></li>
+                            <li><a href="#">Cooking</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">Features</a>
+                        <ul class="m1-navi-menus-subMenu">
+                            <li><a href="#">Why Buy?</a></li>
+                            <li><a href="#">Theme Options</a></li>
+                            <li><a href="#">Coming Soon</a></li>                                    
+                            <li><a href="#">Icons</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Shop</a></li>
+                    <li><a href="#">Contact</a></li>
+
+                </ul>
+            </div>
+
+            <div class="navi-cart">
+                <div class="search">
+                    <i class="fa fa-search icon-search"></i>
+                    <form class="search-panel" method="post">
+                        <input type="search" class="menu-search-text" placeholder="Please input search...">
+                        <button class="fa fa-search menu-search-btn"></button>
+                    </form>
+                </div>
+                <div class="shop-cart">
+                    <a href="#"><i class="fa fa-shopping-bag"></i></a>
+                    <span>0</span>
+                    <div class="list-cart">
+                        <span>No product in the cart</span>
+                    </div>
+                </div>
+                <div id="icon-responsive">
+                    <i class="fa fa-bars"></i>
+                </div>
+            </div>
+        </div>
+        <div id="sub-header" class="layout-full has-background">
+            <div class="meta-header" style="background-color:#582892;background-image:none;">
+                <div class="limit-wrapper">
+                    <div class="meta-header-inside">
+                        <header class="page-header layout-centered" data-progressive-animation="page-title">
+                            <h1 style="color: rgb(255, 157, 171); opacity: 1; transform: matrix(1, 0, 0, 1, 0, 0);" itemprop="headline">
+                            Why Buy the Theme
+                            </h1>
+                            <div class="page-header-line"></div>
+                            <div class="desc" style="color: rgb(255, 157, 171); opacity: 1; transform: matrix(1, 0, 0, 1, 0, 0);">Endless layout possibilities</div>
+                        </header>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </header>
 </div>
