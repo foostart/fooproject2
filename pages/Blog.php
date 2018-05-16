1d9e7185b8a8ed8c33ae3987be5f50dc9afb3274
letathung1996 <pecottu111@gmail.com>
2018-05-16 19:06:32 +0700

    commit

---
+++ pages/Blog.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="vi">
+    <head>
+        <title>Blog</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=100" />
+        <meta http-equiv="REFRESH" content="1800" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-title" content="Tai lieu web" />
+        <meta name="description" content="Tai lieu web" />
+        <meta name="keywords" content="Tai lieu web"/>
+
+        <link href="../1234/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="../1234/css/1234.css" rel="stylesheet" type="text/css"/>
+        <link href="../1248/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+        <link href="../1248/css/1248.css" rel="stylesheet" type="text/css"/>
+        <link href="../1269/css/swiper.min.css" rel="stylesheet" type="text/css"/>
+        <link href="../1269/css/slick-theme.css" rel="stylesheet" type="text/css"/>
+        <link href="../1269/css/slick.css" rel="stylesheet" type="text/css"/>
+        <link href="../1269/css/1269.css" rel="stylesheet" type="text/css"/>
+        <link href="../1244/css/1244.css" rel="stylesheet" type="text/css"/>
+
+        <script src="../1234/js/jquery-2.1.4.min.js" ></script>
+        <script src="../1234/js/1234.js" ></script> 
+        <script src="../1248/js/amination.js"></script> 
+
+   
+       <script src="../1269/js/bootstrap.min.js"></script>  
+        <script src="../1269/js/swiper.min.js"></script>
+        <script src="../1269/js/m1-slide-js.js"></script>
+         <script src="../1269/js/modul1-navi-js.js"></script>
+        <script src="../1269/js/1269.js"></script>
+            
+
+        
+    </head>
+    <body>
+        <?php include '../1234/1234-content.php'; ?>
+
+        <?php include '../1248/1248-content.php'; ?>
+
+        <?php include '../1269/1269-content.php'; ?>
+
+        <?php include '../1244/1244-content.php'; ?>
+    </body>
+</html>
