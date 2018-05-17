eebe8661c83675d24994305f47de78a00e2c38be
alvindragoon <33717480+alvindragoon@users.noreply.github.com>
2018-05-17 10:41:23 +0700

    update 1275

--- 1275/1275-content.php
+++ 1275/1275-content.php
@@ -8,7 +8,7 @@
     $url_path = str_replace('\\', '/', $url_path);
 ?>
 
-        <div class="type-1275">
+    <div class="type-1275">
         <div class="container">
             <!--............-->
             <header class="page-header">
@@ -29,11 +29,7 @@
                         <li ><a data-toggle="tab" href="#f">Body</a></li>
                         <li ><a data-toggle="tab" href="#g">Shooter & Subfooter</a></li>
                         <li ><a data-toggle="tab" href="#h">Site Identiy, Product Archives</a></li>
-                        <li ><a data-toggle="tab" href="#h">Menu, Widget, Static Front Page</a></li>
-                        <li ><a data-toggle="tab" href="#h">Page Options</a></li>
-                        <li ><a data-toggle="tab" href="#h">Blog Post Options</a></li>                        
-                        <li ><a data-toggle="tab" href="#h">Project Post Options</a></li>
-                        <li ><a data-toggle="tab" href="#h">Drag & Drop Builder</a></li>
+                        <li ><a data-toggle="tab" href="#i">Menu, Widget, Static Front Page</a></li>
                     </ul>
                 </div>
                 <!--..-->
@@ -41,7 +37,20 @@
                     <div class="tab-content">
                         
                         <div id="a" class="tab-pane fade in active">
+                            <div class="all">
+
+                                <div>
+                                    <div class="image">
+                                        <img class="img-reponsive" src="<?php echo $url_path ?>/image/imageA.png" alt=""/>
+                                        <img class="img-reponsive" src="<?php echo $url_path ?>/image/imageA.png" alt=""/>
+                                        <img class="img-reponsive" src="<?php echo $url_path ?>/image/imageA.png" alt=""/>
+                                        <img class="img-reponsive" src="<?php echo $url_path ?>/image/imageA.png" alt=""/>
+                                        <img class="img-reponsive" src="<?php echo $url_path ?>/image/imageA.png" alt=""/>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
+                        
                         <div id="b" class="tab-pane "style="background: white;" >
                             <div class="all">
 
@@ -49,17 +58,99 @@
                                     <div class="image">
                                         <img class="img-reponsive" src="<?php echo $url_path ?>/image/content.jpg" alt=""/>
                                     </div>
+                                </div>
                             </div>
                         </div>
-                        <div id="c" class="tab-pane fade in active">
-                        </div>  
+                    
+                        <div id="c" class="tab-pane" style="background: white;">
+                            <div class="all">
 
-                        <div id="d" class="tab-pane fade in active">
+                                <div>
+                                    <div class="image">
+                                        <img class="img-reponsive" src="<?php echo $url_path ?>/image/imageC.png" alt=""/>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
+                        
+
+                        <div id="d" class="tab-pane"    style="background: white;">
+                            <div class="all">
+
+                                <div>
+                                    <div class="image">
+                                        <img class="img-reponsive" src="<?php echo $url_path ?>/image/imageD.png" alt=""/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div id="e" class="tab-pane">
+                            <div class="all">
+
+                            <div><h1 style="padding-bottom: 10px"> Top Bar</h1></div>
+                                <div>
+                                    <div class="image">
+                                        <img class="img-reponsive" src="<?php echo $url_path ?>/image/imageE.png" alt=""/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                     
+                        <div id="f" class="tab-pane">
+                            <div class="all">
+
+                                <div>
+                                    <div class="image">
+                                        <img class="img-reponsive" src="<?php echo $url_path ?>/image/imageF.png" alt=""/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div id="g" class="tab-pane">
+                            <div class="all">
+
+                            <div><h1 style="padding-bottom: 10px"> Footer</h1></div>
+
+                                <div>
+                                    <div class="image">
+                                        <img class="img-reponsive" src="<?php echo $url_path ?>/image/imageG.png" alt=""/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div id="h" class="tab-pane">
+                            <div class="all">
+
+                            <div><h1 style="padding-bottom: 10px">Site Identity</h1></div>
+
+                                <div>
+                                    <div class="image">
+                                        <img class="img-reponsive" src="<?php echo $url_path ?>/image/imageH.png" alt=""/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div id="i" class="tab-pane">
+                            <div class="all">
+
+                            <div><h1 style="padding-bottom: 10px"> Menus</h1></div>
+                                <div>
+                                    <div class="image">
+                                        <img class="img-reponsive" src="<?php echo $url_path ?>/image/imageI.png" alt=""/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
          </div>
      </div>
+
 
 
