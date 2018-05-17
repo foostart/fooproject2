b3035fe02a559e099aa22cccf00c927fd68421c2
vientay1995 <33610286+vientay1995@users.noreply.github.com>
2018-05-17 09:54:15 +0700

    update-theme.php
    update-theme.php

--- pages/Theme.php
+++ pages/Theme.php
@@ -3,20 +3,23 @@
 <head>
 	<title>Theme</title>
 
-    <link href="../1235/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <link href="../1235/css/font-awesome.min.css" rel="stylesheet">
-    <link href="../1235/css/1235.css" rel="stylesheet" type="text/css" />
+    <link href="../1277/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="../1277/css/font-awesome.min.css" rel="stylesheet">
+    <link href="../1277/css/1277.css" rel="stylesheet" type="text/css" />
     <link href="../1248/css/1248.css" rel="stylesheet" type="text/css"/>
+    <link href="../1275/css/1275.css" rel="stylesheet" type="text/css"/>
     <link href="../1244/css/1244.css" rel="stylesheet" type="text/css"/>
 
 
     <script src="../1235/js/jquery-2.1.4.min.js"></script>
 	<script src="../1248/js/amination.js"></script>
-    <script src="../1235/js/1235.js"></script>
+    <script src="../1275/js/1275.js"></script>
+    <script src="../1277/js/1235.js"></script>
 </head>
 <body>
-	<?php include '../1235/1235-content.php'; ?>
+	<?php include '../1277/1277-content.php'; ?>
 	<?php include '../1248/1248-content.php'; ?>
+    <?php include '../1275/1275-content.php'; ?>
 	<?php include '../1244/1244-content.php'; ?>
 </body>
 </html>
